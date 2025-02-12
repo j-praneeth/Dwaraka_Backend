f1aece655b2945a551003382dc395e31ab56885f
J Praneeth <praneeth.janjanam@aceec.ac.in>
2025-02-12 12:43:22 +0530

    commented reset-password route

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -4,14 +4,14 @@ const {
   loginUser,
   logoutUser,
   authMiddleware,
-  resetPassword,
+  // resetPassword,
 } = require("../../controllers/auth/auth-controller");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/reset-password", resetPassword);
+// router.post("/reset-password", resetPassword);
 router.post("/logout", logoutUser);
 router.get("/check-auth", authMiddleware, (req, res) => {
   const user = req.user;
