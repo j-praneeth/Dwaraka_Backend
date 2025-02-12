cb5c215160c02795513d6163df37826e8eb0346d
J Praneeth <praneeth.janjanam@aceec.ac.in>
2025-02-12 11:00:59 +0530

    Added routing for reset pass

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -4,12 +4,14 @@ const {
   loginUser,
   logoutUser,
   authMiddleware,
+  resetPassword,
 } = require("../../controllers/auth/auth-controller");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/reset-password", resetPassword);
 router.post("/logout", logoutUser);
 router.get("/check-auth", authMiddleware, (req, res) => {
   const user = req.user;
