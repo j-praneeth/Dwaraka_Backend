86fe0bfd96c24581a41e0ec920cae5386807f2ca
J Praneeth <praneeth.janjanam@aceec.ac.in>
2025-02-12 13:00:15 +0530

    Update auth-controller.js

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -1,126 +1,3 @@
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const User = require("../../models/User");
-
-// // register
-// const registerUser = async (req, res) => {
-//   const { userName, email, password } = req.body;
-
-//   try {
-//     const checkUser = await User.findOne({ email });
-//     if (checkUser)
-//       return res.json({
-//         success: false,
-//         message:
-//           "User Already exists with the same email! Please try again",
-//       });
-
-//     // Hash the password before saving
-//     const hashPassword = await bcrypt.hash(password, 12);
-//     const newUser = new User({
-//       userName,
-//       email,
-//       password: hashPassword,
-//     });
-
-//     await newUser.save();
-
-//     const { password: _, _id, ...userData } = newUser.toObject();
-
-//     res.status(200).json({
-//       success: true,
-//       message: "Registration successful",
-//       user: userData,
-//     });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({
-//       success: false,
-//       message: "Some error occurred",
-//     });
-//   }
-// };
-
-// // login
-// const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const checkUser = await User.findOne({ email });
-//     if (!checkUser) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "User doesn't exist! Please register first",
-//       });
-//     }
-
-//     // Compare the hashed password
-//     const isMatch = await bcrypt.compare(password, checkUser.password);
-//     if (!isMatch) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "Incorrect password! Please try again",
-//       });
-//     }
-
-//     const token = jwt.sign(
-//       {
-//         id: checkUser._id,
-//         role: checkUser.role,
-//         email: checkUser.email,
-//         userName: checkUser.userName,
-//       },
-//       "CLIENT_SECRET_KEY",
-//       { expiresIn: "60m" }
-//     );
-
-//     const { password: _, _id, ...userData } = newUser.toObject();
-
-//     return res.status(200).json({
-//       success: true,
-//       token,
-//       user: userData,
-//     });
-//   } catch (e) {
-//     console.error("Login error:", e);
-//     return res.status(500).json({
-//       success: false,
-//       message: "Some error occurred",
-//     });
-//   }
-// };
-
-// // logout
-// const logoutUser = (req, res) => {
-//   res.clearCookie("token").json({
-//     success: true,
-//     message: "Logged out successfully!",
-//   });
-// };
-
-// // auth middleware
-// const authMiddleware = async (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token)
-//     return res.status(401).json({
-//       success: false,
-//       message: "Unauthorized user!",
-//     });
-
-//   try {
-//     const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({
-//       success: false,
-//       message: "Unauthorized user!",
-//     });
-//   }
-// };
-
-// module.exports = { registerUser, loginUser, logoutUser, authMiddleware };
-
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
@@ -162,59 +39,61 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
+  // Validate input
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide email and password"
+    });
+  }
+
   try {
     const checkUser = await User.findOne({ email });
-    if (!checkUser)
-      return res.json({
-        success: false,
-        message: "User doesn't exists! Please register first",
-      });
 
-    const checkPasswordMatch = await bcrypt.compare(
-      password,
-      checkUser.password
-    );
-    if (!checkPasswordMatch)
-      return res.json({
+    if (!checkUser) {
+      return res.status(401).json({
         success: false,
-        message: "Incorrect password! Please try again",
+        message: "Invalid credentials"
       });
+    }
 
+    const isPasswordValid = await bcrypt.compare(password, checkUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect password! Please try again"
+      });
+    }
+
+    // Generate JWT token
     const token = jwt.sign(
-      {
-        id: checkUser._id,
-        role: checkUser.role,
-        email: checkUser.email,
-        userName: checkUser.userName,
-      },
-      "CLIENT_SECRET_KEY",
+      { id: checkUser._id, role: checkUser.role, email: checkUser.email },
+      process.env.JWT_SECRET || "CLIENT_SECRET_KEY",
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, { httpOnly: true }).json({
       success: true,
-      message: "Logged in successfully",
+      message: "Login successful",
       user: {
+        id: checkUser._id,
         email: checkUser.email,
         role: checkUser.role,
-        id: checkUser._id,
-        userName: checkUser.userName,
-      },
-      token,
+        userName: checkUser.userName
+      }
     });
-
-    res.status(200).json({ success: true, token, user: checkUser });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured",
-    });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    if (!res.headersSent) {
+      return res.status(500).json({
+        success: false,
+        message: "Login failed"
+      });
+    }
   }
 };
 
 //logout
-
 const logoutUser = (req, res) => {
   res.clearCookie("token").json({
     success: true,
